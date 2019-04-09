8683eb889778f2baf2019533ec9a8fce254964a8
Shane Drower-Copley <bluekable@Shanes-MacBook-Pro.local>
2019-04-09 15:35:29 +0200

    Upgraded post to cart to accept qty's

---
+++ hetzner-catalog-config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://orders.love2learn.co.za/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://orders.love2learn.co.za/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/www/users/lovejk/orders.love2learn.co.za/catalog/');
+define('DIR_SYSTEM', '/usr/www/users/lovejk/orders.love2learn.co.za/system/');
+define('DIR_IMAGE', '/usr/www/users/lovejk/orders.love2learn.co.za/image/');
+define('DIR_STORAGE', '/usr/home/lovejk/opencart/orders/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'dedi903.jnb1.host-h.net');
+define('DB_USERNAME', 'lovejk_12');
+define('DB_PASSWORD', 'g12FXpHuqz8');
+define('DB_DATABASE', 'love2learnorders_db9');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
